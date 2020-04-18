946fb07911f99d70ad8b847564653e90a13a9b64
Ramon <r.peek@student.fontys.nl>
2020-04-18 14:50:32 +0200

    Added empty endpoints for the WolfController

--- src/main/java/nl/ramonpeek/controllers/PackController.java
+++ src/main/java/nl/ramonpeek/controllers/PackController.java
@@ -2,12 +2,16 @@ package nl.ramonpeek.controllers;
 
 import nl.ramonpeek.managers.interfaces.IPackManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/pack")
 public class PackController {
 
     @Autowired
     private IPackManager packManager;
+
+    
 
 }
--- src/main/java/nl/ramonpeek/controllers/WolfController.java
+++ src/main/java/nl/ramonpeek/controllers/WolfController.java
@@ -1,15 +1,49 @@
 package nl.ramonpeek.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import nl.ramonpeek.managers.interfaces.IPackManager;
 import nl.ramonpeek.managers.interfaces.IWolfManager;
+import nl.ramonpeek.models.Location;
+import nl.ramonpeek.models.Wolf;
+import nl.ramonpeek.models.enums.Gender;
+import nl.ramonpeek.models.enums.WolfType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "/wolf", produces = "application/json")
 public class WolfController {
 
     @Autowired
     private IWolfManager wolfManager;
+
+    @ApiOperation(value = "", response = Wolf.class, responseContainer = "List")
+    @GetMapping()
+    public List<Wolf> getAllWolves() {
+        return null;
+    }
+
+    @ApiOperation(value = "", response = Wolf.class)
+    @PostMapping()
+    public Wolf createWolf(Wolf wolf) {
+        return null;
+    }
+
+    @ApiOperation(value = "", response = Wolf.class)
+    @DeleteMapping()
+    public Wolf deleteWolf(Wolf wolf) {
+        return null;
+    }
+
+    @ApiOperation(value = "", response = Wolf.class)
+    @PutMapping
+    public Wolf updateWolf(int wolfId, Wolf updatedWolf) {
+        return null;
+    }
 
 }
