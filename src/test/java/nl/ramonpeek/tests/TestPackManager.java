ce11d9f42e430c77c3d7e843a3e1c07b5eb61311
Ramon <r.peek@student.fontys.nl>
2020-04-18 16:52:29 +0200

    Setup testdata for the testenvironment and added a few unittests

--- src/test/java/nl/ramonpeek/tests/TestPackManager.java
+++ src/test/java/nl/ramonpeek/tests/TestPackManager.java
@@ -1,19 +1,19 @@
 package nl.ramonpeek.tests;
 
 import nl.ramonpeek.dal.memory.PackMemoryContext;
-import nl.ramonpeek.dal.memory.WolfMemoryContext;
 import nl.ramonpeek.managers.PackManager;
-import nl.ramonpeek.managers.WolfManager;
+import nl.ramonpeek.models.Location;
 import nl.ramonpeek.models.Pack;
 import nl.ramonpeek.models.Wolf;
+import nl.ramonpeek.models.enums.Gender;
+import nl.ramonpeek.models.enums.WolfType;
 import nl.ramonpeek.repositories.PackRepo;
-import nl.ramonpeek.repositories.WolfRepo;
-import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class TestPackManager {
 
@@ -23,7 +23,13 @@ public class TestPackManager {
     public void resetTestEnvironment() {
         PackMemoryContext context = new PackMemoryContext();
         context.setMemory(new ArrayList<Pack>() {{
-            //FILL TEST ENVIRONMENT DATA HERE
+            add(new Pack(0, "AlphaPack", new ArrayList<Wolf>() {{
+                add(new Wolf(0, WolfType.BETA, "Jan", "Pieters", Gender.MALE, new Date(), new Location(0, 0)));
+                add(new Wolf(1, WolfType.OMEGA, "Marije", "Janssen", Gender.FEMALE, new Date(), new Location(0, 0)));
+            }}));
+            add(new Pack(1, "BetaPack", new ArrayList<Wolf>() {{
+                add(new Wolf(0, WolfType.BETA, "Jan", "Pieters", Gender.MALE, new Date(), new Location(0, 0)));
+            }}));
         }});
         this.packManager = new PackManager(new PackRepo(context));
     }
@@ -31,18 +37,24 @@ public class TestPackManager {
     @Test
     public void testGetPackByExistingId() {
         //Arrange
+        int packId = 0;
 
         //Act
+        Pack result = packManager.getPackById(packId);
 
         //Assert
+        Assert.assertNotNull(result);
     }
 
     @Test
     public void testGetPackByNonExistingId() {
         //Arrange
+        int packId = 2;
 
         //Act
+        Pack result = packManager.getPackById(packId);
 
         //Assert
+        Assert.assertNull(result);
     }
 }
--- src/test/java/nl/ramonpeek/tests/TestWolfManager.java
+++ src/test/java/nl/ramonpeek/tests/TestWolfManager.java
@@ -2,12 +2,17 @@ package nl.ramonpeek.tests;
 
 import nl.ramonpeek.dal.memory.WolfMemoryContext;
 import nl.ramonpeek.managers.WolfManager;
+import nl.ramonpeek.models.Location;
 import nl.ramonpeek.models.Wolf;
+import nl.ramonpeek.models.enums.Gender;
+import nl.ramonpeek.models.enums.WolfType;
 import nl.ramonpeek.repositories.WolfRepo;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class TestWolfManager {
 
@@ -17,7 +22,8 @@ public class TestWolfManager {
     public void resetTestEnvironment() {
         WolfMemoryContext context = new WolfMemoryContext();
         context.setMemory(new ArrayList<Wolf>() {{
-            //FILL TEST ENVIRONMENT DATA HERE
+            add(new Wolf(0, WolfType.BETA, "Jan", "Pieters", Gender.MALE, new Date(), new Location(0, 0)));
+            add(new Wolf(1, WolfType.OMEGA, "Marije", "Janssen", Gender.FEMALE, new Date(), new Location(0, 0)));
         }});
         this.wolfManager = new WolfManager(new WolfRepo(context));
     }
@@ -25,18 +31,24 @@ public class TestWolfManager {
     @Test
     public void testGetWolfByExistingId() {
         //Arrange
+        int wolfId = 0;
 
         //Act
+        Wolf result = wolfManager.getWolfById(wolfId);
 
         //Assert
+        Assert.assertNotNull(result);
     }
 
     @Test
     public void testGetWolfByNonExistingId() {
         //Arrange
+        int wolfId = 2;
 
         //Act
+        Wolf result = wolfManager.getWolfById(wolfId);
 
         //Assert
+        Assert.assertNull(result);
     }
 }
