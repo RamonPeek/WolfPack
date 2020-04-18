0a16adcb247794461641211c5707fd46b530eef7
Ramon <r.peek@student.fontys.nl>
2020-04-18 15:08:38 +0200

    Added descriptions for swagger and added empty endpoints for the PackController

--- src/main/java/nl/ramonpeek/controllers/PackController.java
+++ src/main/java/nl/ramonpeek/controllers/PackController.java
@@ -1,17 +1,43 @@
 package nl.ramonpeek.controllers;
 
+import io.swagger.annotations.ApiOperation;
 import nl.ramonpeek.managers.interfaces.IPackManager;
+import nl.ramonpeek.models.Pack;
+import nl.ramonpeek.models.Wolf;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/pack")
+@RequestMapping(value = "/pack", produces = "application/json")
 public class PackController {
 
     @Autowired
     private IPackManager packManager;
 
-    
+    @ApiOperation(value = "Get a collection of all packs.", response = Pack.class, responseContainer = "List")
+    @GetMapping()
+    public List<Pack> getAllPacks() {
+        return null;
+    }
+
+    @ApiOperation(value = "Create a pack.", response = Pack.class)
+    @PostMapping()
+    public Pack createPack(@RequestBody Pack pack) {
+        return null;
+    }
+
+    @ApiOperation(value = "Add a wolf to a pack.", response = Pack.class)
+    @PutMapping("{packId}/addWolf")
+    public Pack addWolfToPack(@RequestBody Wolf wolf, @PathVariable("packId") int packId) {
+        return null;
+    }
+
+    @ApiOperation(value = "Remove a wolf from a pack.", response = Pack.class)
+    @PutMapping("{packId}/removeWolf")
+    public Pack removeWolfFromPack(@RequestBody Wolf wolf, @PathVariable("packId") int packId) {
+        return null;
+    }
 
 }
--- src/main/java/nl/ramonpeek/controllers/WolfController.java
+++ src/main/java/nl/ramonpeek/controllers/WolfController.java
@@ -1,18 +1,10 @@
 package nl.ramonpeek.controllers;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import nl.ramonpeek.managers.interfaces.IPackManager;
 import nl.ramonpeek.managers.interfaces.IWolfManager;
-import nl.ramonpeek.models.Location;
 import nl.ramonpeek.models.Wolf;
-import nl.ramonpeek.models.enums.Gender;
-import nl.ramonpeek.models.enums.WolfType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -22,27 +14,27 @@ public class WolfController {
     @Autowired
     private IWolfManager wolfManager;
 
-    @ApiOperation(value = "", response = Wolf.class, responseContainer = "List")
+    @ApiOperation(value = "Get a collection of all wolves.", response = Wolf.class, responseContainer = "List")
     @GetMapping()
     public List<Wolf> getAllWolves() {
         return null;
     }
 
-    @ApiOperation(value = "", response = Wolf.class)
+    @ApiOperation(value = "Create a wolf.", response = Wolf.class)
     @PostMapping()
-    public Wolf createWolf(Wolf wolf) {
+    public Wolf createWolf(@RequestBody Wolf wolf) {
         return null;
     }
 
-    @ApiOperation(value = "", response = Wolf.class)
+    @ApiOperation(value = "Delete a wolf.", response = Wolf.class)
     @DeleteMapping()
-    public Wolf deleteWolf(Wolf wolf) {
+    public Wolf deleteWolf(@RequestBody Wolf wolf) {
         return null;
     }
 
-    @ApiOperation(value = "", response = Wolf.class)
-    @PutMapping
-    public Wolf updateWolf(int wolfId, Wolf updatedWolf) {
+    @ApiOperation(value = "Update a wolf.", response = Wolf.class)
+    @PutMapping("{wolfId}")
+    public Wolf updateWolf(@PathVariable("wolfId") int wolfId, @RequestBody Wolf updatedWolf) {
         return null;
     }
 
