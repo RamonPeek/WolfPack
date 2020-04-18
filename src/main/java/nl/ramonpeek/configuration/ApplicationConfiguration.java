4332f4226a85792ec8a089b0b120aa125690cd1f
Ramon <r.peek@student.fontys.nl>
2020-04-18 14:22:04 +0200

    Configured swagger

--- src/main/java/nl/ramonpeek/configuration/ApplicationConfiguration.java
+++ src/main/java/nl/ramonpeek/configuration/ApplicationConfiguration.java
@@ -10,6 +10,11 @@ import nl.ramonpeek.repositories.PackRepo;
 import nl.ramonpeek.repositories.WolfRepo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class ApplicationConfiguration {
--- src/main/java/nl/ramonpeek/controllers/WolfController.java
+++ src/main/java/nl/ramonpeek/controllers/WolfController.java
@@ -1,4 +1,14 @@
 package nl.ramonpeek.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WolfController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hi";
+    }
+
 }
