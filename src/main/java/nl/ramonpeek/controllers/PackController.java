543735b536d47dec34876702a77adcaf6ca38d18
Ramon <r.peek@student.fontys.nl>
2020-04-18 14:23:18 +0200

    Added references to logiclayer in the controllers

--- src/main/java/nl/ramonpeek/controllers/PackController.java
+++ src/main/java/nl/ramonpeek/controllers/PackController.java
@@ -1,7 +1,13 @@
 package nl.ramonpeek.controllers;
 
+import nl.ramonpeek.managers.interfaces.IPackManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PackController {
+
+    @Autowired
+    private IPackManager packManager;
+
 }
--- src/main/java/nl/ramonpeek/controllers/WolfController.java
+++ src/main/java/nl/ramonpeek/controllers/WolfController.java
@@ -1,14 +1,15 @@
 package nl.ramonpeek.controllers;
 
+import nl.ramonpeek.managers.interfaces.IPackManager;
+import nl.ramonpeek.managers.interfaces.IWolfManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WolfController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "hi";
-    }
+    @Autowired
+    private IWolfManager wolfManager;
 
 }
